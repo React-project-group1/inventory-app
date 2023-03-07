8d7ab8f83e2c2a91b4b5cfd975e703619f10f746
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-07 15:53:14 +0000

    Back button added to ItemDetails

--- public/react/components/ItemDetails.js
+++ public/react/components/ItemDetails.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 export const ItemDetails = (props) => {
+
+    const getAllItems = () => {
+        props.setItemDetails();
+    }
+
     return <>
-        <h3>{props.item.title}</h3>
-        <p>{props.item.price}</p>
-        <p>{props.item.category}</p>
-        <p>{props.item.description}</p>
-        <img src={props.item.image} alt={props.item.title} />
+        <div>
+            <h3>{props.item.title}</h3>
+            <p>{props.item.price}</p>
+            <p>{props.item.category}</p>
+            <p>{props.item.description}</p>
+            <img src={props.item.image} alt={props.item.title} />
+       </div>
+       <button onClick={getAllItems}>View all Items</button>
     </>
 }
