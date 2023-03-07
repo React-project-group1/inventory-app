ddd084829b235bd44a542485dfba78b1cb0f39c4
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-07 10:34:57 +0000

    Mapping all item data

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,29 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
-
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+	const [items, setItems] = useState([]);
 
 	async function fetchItems(){
 		try {
 			const response = await fetch(`${apiURL}/items`);
 			const itemsData = await response.json();
 			
+			setItems(itemsData)
 			console.log(itemsData)
 		} catch (err) {
 			console.log("Oh no an error! ", err)
@@ -31,7 +21,7 @@ export const App = () => {
 	}
 
 	useEffect(() => {
-		fetchSauces();
+		fetchItems();
 	}, []);
 
 	return (
@@ -39,7 +29,7 @@ export const App = () => {
       <h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
 			<button onClick={fetchItems}>GET ALL ITEMS</button>
-			<SaucesList sauces={sauces} />
+			<ItemsList items={items} />
 		</main>
 	)
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router();
+// const router = express.Router();
 const itemRouter = express.Router();
 
 // different model routers
-router.use('/sauces', require('./sauces'));  // sauces
+// router.use('/sauces', require('./sauces'));  // sauces
 itemRouter.use('/items', require('./items'))    // items
 
-module.exports = router;
+module.exports = itemRouter;
