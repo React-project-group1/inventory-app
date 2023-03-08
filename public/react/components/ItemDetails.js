50183a9246518533d76a6efae797ca4295ecc694
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-08 11:24:36 +0000

    Delete button/function added

--- public/react/components/ItemDetails.js
+++ public/react/components/ItemDetails.js
@@ -1,9 +1,26 @@
 import React from "react";
+import apiURL from "../api";
 
 export const ItemDetails = (props) => {
 
     const getAllItems = () => {
         props.setItemDetails();
+    }
+
+    async function onDelete() {
+        try {
+            const res = await fetch(`${apiURL}/items/${props.item.id}`, {
+                method: "DELETE"
+            });
+            await res.json();
+            if(res.ok) {
+                props.setItemDetails();
+            } else {
+                throw new Error('Something went wrong!')
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     return <>
@@ -15,5 +32,6 @@ export const ItemDetails = (props) => {
             <img src={props.item.image} alt={props.item.title} />
        </div>
        <button onClick={getAllItems}>View all Items</button>
+       <button onClick={onDelete}>DELETE</button>
     </>
 }
